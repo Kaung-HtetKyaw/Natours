ce030d1648fc905594df6c11ecf9b0bbb8b3b2e0
Kaung Htet Kyaw <kaunghtetkyaw02749@gmail.com>
2020-10-19 22:04:53 +0630

    Erros during development vs production

--- controller/error.js
+++ controller/error.js
@@ -1,8 +1,35 @@
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "error";
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV == "development") {
+    sendErrorDev(error, res);
+  } else if (process.env.NODE_ENV == "production") {
+    sendErrorProd(error, res);
+  }
 };
+
+function sendErrorDev(error, res) {
+  res.status(error.statucCode).json({
+    status: status.error,
+    message: error.message,
+    error: error,
+    stack: error.stack,
+  });
+}
+function sendErrorProd(error, res) {
+  // catch the operational errors
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+  // catch Programming,unexpected errors
+  else {
+    console.error("Error ❎", error);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+}
