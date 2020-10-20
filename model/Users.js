2ecffbd16c64c1d86fe6ba73057d2f06ae9ca17e
Kaung Htet Kyaw <kaunghtetkyaw02749@gmail.com>
2020-10-20 23:31:45 +0630

    Managing Passwords

--- model/Users.js
+++ model/Users.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -34,6 +35,14 @@ const userSchema = new mongoose.Schema({
       message: "Password don't match",
     },
   },
+});
+
+// plain text password to encrypted password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmedPassword = undefined; // removing because dont need it anymore
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
