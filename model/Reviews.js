fcbe7f26d531fb0c37f775a8ab0810e44f7b62fb
Kaung Htet Kyaw <kaunghtetkyaw02749@gmail.com>
2020-10-27 00:57:14 +0630

    Modelling Review

---
+++ model/Reviews.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const options = {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+};
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: true,
+      message: "Please write a review for this tour.",
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please rate this tour"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Please provide a tour which this review belongs to"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Please provide an author"],
+    },
+  },
+  options
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
