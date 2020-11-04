7c5c90adec6a5eaf04239cada5a811aa44cd2607
Kaung Htet Kyaw <kaunghtetkyaw02749@gmail.com>
2020-11-05 02:49:14 +0630

    bundle

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9116,7 +9116,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45705" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39653" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
